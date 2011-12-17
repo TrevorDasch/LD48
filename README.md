9710f544ff775b202ca22e8d99d3335da7e82368
Trevor Dasch <trevor.dasch@gmail.com>
2011-12-17 00:10:46 -0800

    we have controls!

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ I have approximately 1 day and 20 hours remaining. I believe I should begin.
 
 [Fri 9:44 PM]
 My Fedora box is being a jerk, so I'm going home to work on ubuntu. Also, hello world.
+
+[Sat 12:06 AM]
+Started working with lua and love. Love is pretty simple and easy to use, which is awesome. Lua isn't so much different from javascript really, except objects don't appear to be native, though its not to difficult to implement apparently. Right now I've got a ship moving around, and swinging around to face the mouse. Need to fix the transparency on the image.
+
--- main.lua
+++ main.lua
@@ -1,3 +1,91 @@
-function love.draw()
-	love.graphics.print("Hello World", 400,300)
+player = {}
+	
+player.posX = 300
+player.posY = 300
+
+player.lookX = 300
+player.lookY = 300
+
+player.firing = false
+
+player.health = 100
+
+player.update = function(v,h,x,y)
+	player.posX= player.posX+h
+	player.posY= player.posY+v
+	player.lookX= x
+	player.lookY= y
 end
+
+player.fire = function()
+	player.firing = true
+end
+
+player.endfire = function()
+	player.firing = false
+end
+
+player.draw = function()
+	
+	local r = math.atan2(player.lookY-player.posY,player.lookX-player.posX) + math.pi /2
+	love.graphics.draw(player.image, player.posX, player.posY, r, 1, 1, 32, 32)
+	
+end
+
+
+function love.load()
+	player.image = love.graphics.newImage("player.png")
+	--enemyImages = {}
+	--for i = 1,10 do
+	--	enemyImages[i] = love.graphics.newImage("enemy-"+i+".png")
+	--end
+end
+
+
+
+
+
+function love.draw()
+	if player.firing then
+		love.graphics.print("firing", 400,300)
+	else
+		love.graphics.print("not firing", 400,300)
+	end
+	player.draw()
+end
+
+function love.mousepressed(x, y, button)
+	if button == 'l' then
+		player.fire()
+	end
+end
+
+function love.mousereleased(x, y, button)
+	if button == 'l' then
+		player.endfire()
+	end
+end
+
+function love.update()
+	local v = 0
+	local h = 0
+	if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
+		v = v - 4
+	end
+	if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
+		v = v + 4
+	end
+	if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+		h = h + 4
+	end
+	if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+		h = h - 4
+	end
+	
+	local x = love.mouse.getX()
+	local y = love.mouse.getY()
+	
+	player.update(v,h,x,y)
+	
+end
+	
